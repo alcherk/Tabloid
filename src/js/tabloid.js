08564f8cbafa724ca8cb8fb66633fabff34694bf
Aleksey Cherkasskiy <alcherk@mail.ru>
2013-09-06 11:20:43 +0400

    added update tabloid on month change

--- src/js/tabloid.js
+++ src/js/tabloid.js
@@ -1,4 +1,7 @@
 (function () {
+	var tabloid = [2, 2, 0, 0, 1, 1, 0, 0],
+		start_date = new Date('2013-09-05');
+
     var $calendar = $( '#calendar' ),
 	    cal = $calendar.calendario( {
 	            onDayClick : function( $el, $contentEl, dateProperties ) {
@@ -23,19 +26,21 @@
     function updateMonthYear() {                
         $month.html( cal.getMonthName() );
         $year.html( cal.getYear() );
+        updateTabloid(4);
     }
 
+    function updateTabloid(delta) {
+	    var $date_cells = $('.fc-date').parent(), i;
 
-    var $date_cells = $('.fc-date').parent(), i;
+	    var classes = ['tabloid-cell-rest', 'tabloid-cell1', 'tabloid-cell2'],	    	
+	    	month_delta = delta;
 
-    var tabloid = [2, 2, 0, 0, 1, 1, 0, 0],
-    	classes = ['tabloid-cell-rest', 'tabloid-cell1', 'tabloid-cell2'],
-    	start_date = new Date('2013-09-05'),
-    	month_delta = 4;
-
-    for (i = 0, last = $date_cells.length; i < last; i++) {
-    	var tabloid_ind = (i + month_delta) % tabloid.length;
-    	$($date_cells[i]).addClass(classes[tabloid[tabloid_ind]]);
+	    for (i = 0, last = $date_cells.length; i < last; i++) {
+	    	var tabloid_ind = (i + month_delta) % tabloid.length;
+	    	$($date_cells[i]).addClass(classes[tabloid[tabloid_ind]]);
+    	}
     }
+
+    updateTabloid(4);
 
 }());
